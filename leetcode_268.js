2508f4a46de9e87dec0f1af689db6a4d2f334315
ekrambd <ekramhossainekram28@gmail.com>
2026-03-28 12:54:18 +0600

    leetcode 268 solved

---
+++ leetcode_268.js
@@ -0,0 +1,20 @@
+//268. Missing Number
+
+var missingNumber = function(nums) {
+    let actualSum = 0;
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++)
+    {
+        actualSum+=nums[i];
+    }
+    for(let i = 0; i <= nums.length; i++)
+    {
+        sum+=i;
+    }    
+    let result = sum - actualSum;
+    return result;     
+};
+
+console.log(missingNumber([3,0,1]));
+console.log(missingNumber([0,1]));
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]));
