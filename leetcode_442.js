4c6ddc6f07aa363b6b8feedb26bd8dc3cf3dce8d
ekrambd <ekramhossainekram28@gmail.com>
2026-03-28 13:05:41 +0600

    leetcode 442 solved

---
+++ leetcode_442.js
@@ -0,0 +1,17 @@
+//442. Find All Duplicates in an Array
+
+var findDuplicates = function(nums){
+    let map = new Map();
+    let results = [];
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(map.has(nums[i])){
+            results.push(nums[i]);
+        }
+        map.set(nums[i],i);
+    }
+    return results;    
+};
+console.log(findDuplicates([4,3,2,7,8,2,3,1]));
+console.log(findDuplicates([1,1,2]));
+console.log(findDuplicates([1]));
