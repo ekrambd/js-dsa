6032e5b1a54b07d364062487fadd22c5f8e96ca6
ekrmabd <ekramhossainekram28@gmail.com>
2026-04-09 18:19:38 +0600

    leetcode 128 bug fixed

--- leetcode_128.js
+++ leetcode_128.js
@@ -2,22 +2,22 @@
 
 var longestConsecutive = function(nums) {
     let set = new Set(nums);
-    let arr = Array.from(nums);
+    let arr = Array.from(set);
     let longest = 0;
     for(let i = 0; i < arr.length; i++)
     {
         let num = arr[i];
         if(!set.has(num-1)){
-            let current = 1;
-            let count = 1;
+            current = num;
+            count = 1;
             while(set.has(current+1)){
                 current++;
                 count++;
             }
-            longest = Math.max(longest,current);
+            longest = Math.max(longest,count);
         }
     }
-    return longest;           
+    return longest;               
 };
 
 console.log(longestConsecutive([100,4,200,1,3,2]));
